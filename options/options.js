9770dc036e8806a9498c55bbb9c4017c82c12768
Crowdbotics <team@crowdbotics.com>
2021-08-03 03:15:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wispy_block_29368Identifier",
+  url: "https://wispy_block_29368Identifier.botics.co",
+  api: "https://wispy_block_29368Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
